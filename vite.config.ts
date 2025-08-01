65fdd31b4c66409309e09eae427ea5017086c861
qingshanls <y3205731642@outlook.com>
2025-08-01 16:27:58 +0800

    -js

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import Components from 'unplugin-vue-components/vite'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  base: "/",
+  base: "/qsls-home/",
   plugins: [
     vue(),
     AutoImport({
